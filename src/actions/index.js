6c59812987ec985009bbc90b95324cf7972ac9b3
Lazaro21 <olsf21@gmail.com>
2022-07-17 16:28:33 -0300

    creating async actions with react thunk, using lodash to control the amount of requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,38 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = (id) => async (dispatch, getState) => {
+	await dispatch(fetchPosts());
+
+	_.chain(getState().posts)
+		.map("userId")
+		.uniq.forEach((id) => dispatch(id))
+		.value();
+};
 
 export const fetchPosts = () => {
 	return async (dispatch) => {
-		const response = await jsonPlaceholder.get('/posts');
+		const response = await jsonPlaceholder.get("/posts");
 
-		dispatch({ type: "FETCH_POSTS", payload: response });
+		dispatch({ type: "FETCH_POSTS", payload: response.data });
+	};
+};
+
+// export const fetchUser = (id) => (dispatch) => {
+// 	_fetchUser(id, dispatch);
+
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+// 	const response = await jsonPlaceholder.get(`/users/${id}`);
+
+// 		dispatch({ type: "FETCH_USER", payload: response.data });
+// })
+
+export const fetchUser = (id) => {
+	return async (dispatch) => {
+		const response = await jsonPlaceholder.get(`/users/${id}`);
+
+		dispatch({ type: "FETCH_USER", payload: response.data });
 	};
 };
---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,10 @@
+const listPosts = (state = [], action) => {
+	switch (action.type) {
+		case "FETCH_POSTS":
+			return action.payload;
+		default:
+			return state;
+	}
+};
+
+export default listPosts;
